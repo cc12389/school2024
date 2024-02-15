b3d96ac32f8d591aa4380b91f3969b04f2595f3e
cc12389 <141701543+cc12389@users.noreply.github.com>
2024-02-15 22:53:11 +0800

    Add files via upload

---
+++ camera_js_app/camera_js.js
@@ -0,0 +1,51 @@
+var frontCamera = false;
+var currentSteam;
+
+const
+    cameraView = document.querySelector("#camera-view"),
+    cameraDevice = document.querySelector("#camera-device"),
+    photoDisplay = document.querySelector("#photo-display"),
+    takePhotoButton = document.querySelector("#take-photo-button"),
+    frontCameraButton = document.querySelector("front-camera-button");
+
+function cameraStart(){
+    if (typeof currentSteam != 'undefined'){
+        currentSteam.getTracks().forEach(track => {
+            track.stop();
+        });
+    }
+
+    var constraints = {video:{facingMode: (frontCamera? "user": "environment")}, audio:false};
+
+    navigator.mediaDevices
+        .getUserMedia(constraints)
+        .then(function(stream){
+            currentSteam = stream;
+            cameraDevice.srcObject = stream;
+        })
+        .catch(function(error){
+            console.error("Error happened.",error);
+        });
+
+};
+
+takePhotoButton.onclick = function(){
+    cameraView.clientWidth = cameraDevice.videoWidth;
+    cameraView.clientHeight = cameraDevice.videoHeight;
+    cameraView.getContext("2d").drawImage(cameraDevice,0,0);
+    photoDisplay.src = cameraView.toDataURL("image/webp");
+    photoDisplay.classList.add("photo-taken");
+
+};
+frontCameraButton.onclick = function(){
+    frontCamera = !frontCamera;
+    if(frontCamera){
+        frontCameraButton.textContent = "Back Camera";
+    } else{
+        frontCameraButton.textContent = "Front Camera";
+    }
+    cameraStart();
+}
+
+window.addEventListener("load", cameraStart);
+
